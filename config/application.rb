425a790827932f9ac9a854ab20b944a6c12ffb6b
capsulepod <Capsulepod@gmail.com>
2014-03-25 10:41:48 -0400

    Add asset.precomile < contact.css to application.rb
    config.assets.precompile += %w( *.js *.css ) line added to
    application.br file

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+config.assets.precompile += %w( contact.css )
 
 module DharmaWorld
   class Application < Rails::Application
